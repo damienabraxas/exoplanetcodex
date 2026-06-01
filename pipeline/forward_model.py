155d136d9972d462b16832d3efc9bb384a47267d
damienabraxas <40283020+damienabraxas@users.noreply.github.com>
2026-06-01 00:38:01 -0600

    Create forward_model.py
    physics engine wrapper

---
+++ pipeline/forward_model.py
@@ -0,0 +1,11 @@
+def predict_ews(params, line_list):
+    """
+    params = {Teff, logg, vturb, abundances...}
+
+    returns:
+        EW_model for each line
+    """
+    # 1. write MOOG input
+    # 2. run MOOG
+    # 3. parse output
+    return ew_model
